ce9b93ff1b193a6f2ef1ebd76f85cd9398f151aa
kashaf29 <k.kashafali29@gmail.com>
2023-09-10 19:25:57 +0530

    completing golang jwt authentication

---
+++ routes/authRoutes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	controller "github.com/kashaf29/golang-jwt-project/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("user/login", controller.Login())
+}
